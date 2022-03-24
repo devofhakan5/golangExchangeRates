dad84baf4bb5c13d9d873a659feeb557b2a64d76
Hakan YILMAZ <61453780+devofhakan5@users.noreply.github.com>
2022-03-24 21:16:27 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://www.floatrates.com/daily/usd.xml")
+	if err != nil {
+		log.Fatal("ERROR :%s", err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	var i channel
+	xml.Unmarshal(data, &i)
+	fmt.Println(i.Items)
+}
+
+type items struct {
+	XMLName xml.Name `xml:"item"`
+	DESC    string   `xml:"description"`
+	TITLE   string   `xml:"title"`
+}
+type channel struct {
+	XMLName xml.Name `xml:"channel"`
+	Items   []items  `xml:"item"`
+}
+
+func (x items) String() string {
+	return fmt.Sprintf("\t Description:%s --Title:%s \n", x.DESC, x.TITLE)
+}
